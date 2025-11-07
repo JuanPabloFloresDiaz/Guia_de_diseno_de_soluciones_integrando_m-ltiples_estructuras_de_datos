e661e0a7857cd0442a3970ecc5085fe2991c9f01
JuanPabloFloresDiaz <pablojuanfd@gmail.com>
2025-11-07 10:26:34 -0600

    ✨ Proyecto subido

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Módulo utils
+Contiene utilidades para validación y entrada de datos.
+"""
+
+from .Validaciones import Validaciones
+from .Teclado import Teclado
+
+__all__ = ['Validaciones', 'Teclado']
